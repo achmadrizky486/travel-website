2fcf9ce5ca5a00c195ddf504d25169879d0e48b8
achmadrizky486 <achmadrizky486@gmail.com>
2022-06-03 16:31:15 +0700

    feat : add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "./components/Carousel";
 import Destination from "./components/Destination";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
@@ -15,6 +16,7 @@ function App() {
       <Search />
       <Selects />
       <Carousel />
+      <Footer />
     </div>
   );
 }
